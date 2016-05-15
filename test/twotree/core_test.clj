3b4b46fab6781feb4d936aff29b02c2e661636a5
Jordan Biserkov <jordan@biserkov.com>
2016-05-15 15:49:45 +0300

    Moved the rest of the tests.

--- test/twotree/core_test.clj
+++ test/twotree/core_test.clj
@@ -6,7 +6,7 @@
 
 
 (use 'clojure.pprint)
-(defspec proba 100
+#_(defspec proba 100
          (prop'/for-all [n gen/s-pos-int :when (>= n 3)]
                         (let [t (Generate2tree n)]
                           (pprint t)
@@ -14,3 +14,98 @@
                                 a (longest-path t)
                                 b (longest-path-linear t)]
                             (= a b)))))
+
+(defn max2DistinctFolios [a b k]
+  (reduce max (for [i (range 0 k)
+                    j (range 0 k)
+                    :when (not= i j)]
+                (+ (nth a i) (nth b j)))))
+
+(defn max3DistinctFolios [a b c k]
+  (reduce max (for [i (range 0 k)
+                    j (range 0 k)
+                    t (range 0 k)
+                    :when (distinct? i j t)]
+                (+ (nth a i) (nth b j) (nth c t)))))
+
+(defspec max3-finds-max 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                         v (gen/vector gen/pos-int k)]
+                        (= (first (max3 v k))
+                           (apply max v))))
+(defspec max3-maxes-are-correctly-ordered 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                         v (gen/vector gen/pos-int k)]
+                        (let [[m s t i j] (max3 v k)]
+                          (>= m s t))))
+
+
+
+(defspec max3-max-index-is-correct 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                         v (gen/vector gen/pos-int k)]
+                        (let [[m s t i j] (max3 v k)]
+                          (= (nth v i) m))))
+
+(defspec max3-max2-index-is-correct 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                         v (gen/vector gen/pos-int k)]
+                        (let [[m s t i j] (max3 v k)]
+                          (= (nth v j) s))))
+
+(defspec max3-max2-index-is-correct 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                         v (gen/vector gen/pos-int k)]
+                        (let [[m s t i j] (max3 v k)]
+                          (= (nth v j) s))))
+
+(defspec max3-max2-indicies-are-distinct 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                         v (gen/vector gen/pos-int k)]
+                        (let [[m s t i j] (max3 v k)]
+                          (not= i j))))
+
+(defspec linear-max2DistinctFolios-equals-naive-implementation 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 2)
+                         v1 (gen/vector gen/pos-int k)
+                         v2 (gen/vector gen/pos-int k)]
+                        (= (linear-max2DistinctFolios v1 v2 k)
+                           (max2DistinctFolios v1 v2 k))))
+
+#_(defspec linear-max3DistinctFolios-equals-naive-implementation 1000
+           (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                           v1 (gen/vector gen/pos-int k)
+                           v2 (gen/vector gen/pos-int k)
+                           v3 (gen/vector gen/pos-int k)]
+                          (= (linear-max3DistinctFolios v1 v2 v3 k)
+                             (max3DistinctFolios v1 v2 v3 k))))
+
+(defspec max2DistinctFolios-less-than-sum-max 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 2)
+                         v1 (gen/vector gen/pos-int k)
+                         v2 (gen/vector gen/pos-int k)]
+                        (<= (linear-max2DistinctFolios v1 v2 k)
+                            (+ (apply max v1)
+                               (apply max v2)))))
+
+(defspec max2DistinctFolios-symmetry 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 2)
+                         v1 (gen/vector gen/pos-int k)
+                         v2 (gen/vector gen/pos-int k)]
+                        (= (linear-max2DistinctFolios v1 v2 k)
+                           (linear-max2DistinctFolios v2 v1 k))))
+
+
+(defspec max2DistinctFolios-greather-than-each-max 1000
+         (prop'/for-all [k gen/s-pos-int :when (>= k 2)
+                         v1 (gen/vector gen/pos-int k)
+                         v2 (gen/vector gen/pos-int k)]
+                        (let [m (linear-max2DistinctFolios v1 v2 k)]
+                          (and (>= m (apply max v1))
+                               (>= m (apply max v2))))))
+
+
+
+(defspec longest-path-is-not-crazy 1000
+         (prop'/for-all [k gen/s-pos-int :when (> k 1)]
+                        (< 0 (longest-path (Generate2tree k)) k)))
