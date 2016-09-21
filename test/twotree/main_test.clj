f3ba6ad8ccf5a8bef702282c8e93d12a321d4322
Jordan Biserkov <jordan@biserkov.com>
2016-09-21 14:43:33 +0300

    Extract common functionality. Bring back direct implementation.

--- test/twotree/main_test.clj
+++ test/twotree/main_test.clj
@@ -2,37 +2,31 @@
   (:require [twotree.benchmarks :refer :all]
             [twotree.iterative :refer [longest-path-iterative]]
             [twotree.preprocessed :refer [longest-path-linear]]
+            [twotree.direct :refer [longest-path-direct]]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [com.gfredericks.test.chuck.properties :as prop']))
 
 (def num_tests (int 1e4))
 
+(defn paths [generator n]
+  (if (> n 2)
+    (let [t (generator n)
+          a (longest-path-direct t)
+          b (longest-path-linear t)
+          c (longest-path-iterative (:data t))]
+      (and (= a b c)
+           (< 1 a n)))
+    true))
+
 (defspec random-2trees num_tests
          (prop'/for-all [n gen/s-pos-int]
-                        (if (> n 2)
-                          (let [t (generate-random-2tree n)
-                                b (longest-path-linear t)
-                                c (longest-path-iterative (:data t))]
-                            (= b c))
-                          true)))
+                        (paths generate-random-2tree n)))
 
 (defspec min-internal-edges-2trees num_tests
          (prop'/for-all [n gen/s-pos-int]
-                        (if (> n 2)
-                          (let [t (generate-min-internal-edges-2tree n)
-                                b (longest-path-linear t)
-                                c (longest-path-iterative (:data t))]
-                            (= b c))
-                          true)))
+                        (paths generate-min-internal-edges-2tree n)))
 
 (defspec max-internal-edges-2trees num_tests
          (prop'/for-all [n gen/s-pos-int]
-                        (if (> n 2)
-                          (let [t (generate-max-internal-edges-2tree n)
-                                b (longest-path-linear t)
-                                c (longest-path-iterative (:data t))]
-                            (and
-                              (= b c)
-                              (< 0 b n)))
-                          true)))
+                        (paths generate-max-internal-edges-2tree n)))
