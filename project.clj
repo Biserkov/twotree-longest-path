1d880bfb18c6151e09bbe5300ab91a0da7c46f66
Jordan Biserkov <jordan@biserkov.com>
2016-09-25 17:42:23 +0300

    Upgrade to latest alpha. Remove fressian.

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
                  [criterium "0.4.4"]
-                 [org.clojure/data.fressian "0.2.1"]
                  [org.clojure/data.int-map "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [com.gfredericks/test.chuck "0.2.6"]
