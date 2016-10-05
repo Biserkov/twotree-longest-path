7d7d7567bf1f7d7b753699c66cb508a555dcbe1b
Jordan Biserkov <jordan@biserkov.com>
2016-10-05 19:23:02 +0300

    Simplify main tests. Remove dependency on test.chuck.

--- test/twotree/main_test.clj
+++ test/twotree/main_test.clj
@@ -4,8 +4,8 @@
             [twotree.preprocessed :refer [longest-path-linear]]
             [twotree.direct :refer [longest-path-direct]]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test.check.generators :as gen]
-            [com.gfredericks.test.chuck.properties :as prop']))
+            [clojure.test.check.generators :refer [s-pos-int]]
+            [clojure.test.check.properties :refer [for-all]]))
 
 (def num_tests (int 1e4))
 
@@ -20,13 +20,13 @@
     true))
 
 (defspec random-2trees num_tests
-         (prop'/for-all [n gen/s-pos-int]
-                        (paths generate-random-2tree n)))
+         (for-all [n s-pos-int]
+                  (paths generate-random-2tree n)))
 
 (defspec min-internal-edges-2trees num_tests
-         (prop'/for-all [n gen/s-pos-int]
-                        (paths generate-min-internal-edges-2tree n)))
+         (for-all [n s-pos-int]
+                  (paths generate-min-internal-edges-2tree n)))
 
 (defspec max-internal-edges-2trees num_tests
-         (prop'/for-all [n gen/s-pos-int]
-                        (paths generate-max-internal-edges-2tree n)))
+         (for-all [n s-pos-int]
+                  (paths generate-max-internal-edges-2tree n)))
