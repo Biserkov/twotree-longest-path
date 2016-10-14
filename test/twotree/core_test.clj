5d866f8fb5d0b030a817c1f92c0f67bf846374ab
Jordan Biserkov <jordan@biserkov.com>
2016-10-14 15:52:35 +0300

    Reorder some tests, remove unnecessary requires.

--- test/twotree/core_test.clj
+++ test/twotree/core_test.clj
@@ -1,34 +1,16 @@
 (ns twotree.core-test
   (:require [twotree.core :refer :all]
-            [twotree.iterative :refer [longest-path-iterative]]
-            [twotree.generators :refer :all]
-            [clojure.data.int-map :as set]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [com.gfredericks.test.chuck.properties :as prop']))
 
-
 (use 'clojure.pprint)
-
-
-(defn naive-max2DistinctFolios [a b k]
-  (reduce max (for [i (range 0 k)
-                    j (range 0 k)
-                    :when (not= i j)]
-                (+ (nth a i) (nth b j)))))
-
-(defn naive-max3DistinctFolios [a b c k]
-  (reduce max (for [i (range 0 k)
-                    j (range 0 k)
-                    t (range 0 k)
-                    :when (distinct? i j t)]
-                (+ (nth a i) (nth b j) (nth c t)))))
 
 (defspec max3-finds-max 1000
          (prop'/for-all [k gen/s-pos-int :when (>= k 3)
-                           v (gen/vector gen/pos-int k)]
-                          (= (first (max3 v k))
-                             (apply max v))))
+                         v (gen/vector gen/pos-int k)]
+                        (= (first (max3 v k))
+                           (apply max v))))
 
 (defspec max3-maxes-are-correctly-ordered 1000
          (prop'/for-all [k gen/s-pos-int :when (>= k 3)
@@ -54,20 +36,18 @@
                         (let [[m s t i j] (max3 v k)]
                           (not= i j))))
 
+(defn naive-max2DistinctFolios [a b k]
+  (reduce max (for [i (range 0 k)
+                    j (range 0 k)
+                    :when (not= i j)]
+                (+ (nth a i) (nth b j)))))
+
 (defspec max2DistinctFolios-equals-naive-implementation 1000
          (prop'/for-all [k gen/s-pos-int :when (>= k 2)
                          v1 (gen/vector gen/pos-int k)
                          v2 (gen/vector gen/pos-int k)]
                         (= (max2DistinctFolios v1 v2 k)
                            (naive-max2DistinctFolios v1 v2 k))))
-
-#_(defspec max3DistinctFolios-equals-naive-implementation 1000
-           (prop'/for-all [k gen/s-pos-int :when (>= k 3)
-                           v1 (gen/vector gen/pos-int k)
-                           v2 (gen/vector gen/pos-int k)
-                           v3 (gen/vector gen/pos-int k)]
-                          (= (max3DistinctFolios v1 v2 v3 k)
-                             (naive-max3DistinctFolios v1 v2 v3 k))))
 
 (defspec max2DistinctFolios-less-than-sum-max 1000
          (prop'/for-all [k gen/s-pos-int :when (>= k 2)
@@ -91,4 +71,24 @@
                         (let [m (max2DistinctFolios v1 v2 k)]
                           (and (>= m (apply max v1))
                                (>= m (apply max v2))))))
+
+
+
+
+(defn naive-max3DistinctFolios [a b c k]
+  (reduce max (for [i (range 0 k)
+                    j (range 0 k)
+                    t (range 0 k)
+                    :when (distinct? i j t)]
+                (+ (nth a i) (nth b j) (nth c t)))))
+
+(defspec max3DistinctFolios-equals-naive-implementation 100
+           (prop'/for-all [k gen/s-pos-int :when (>= k 3)
+                           v1 (gen/vector gen/pos-int k)
+                           v2 (gen/vector gen/pos-int k)
+                           v3 (gen/vector gen/pos-int k)]
+                          (= (max3DistinctFolios v1 v2 v3 k)
+                             (naive-max3DistinctFolios v1 v2 v3 k))))
+
+
 
