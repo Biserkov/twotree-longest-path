a0efbb030b219bb016c077871d26c24e39b7cf17
Jordan Biserkov <jordan@biserkov.com>
2016-10-14 15:51:08 +0300

    Extract function. Using the explicit order, lookup only once in EdgeLabels.

--- src/twotree/iterative.clj
+++ src/twotree/iterative.clj
@@ -1,9 +1,18 @@
 (ns twotree.iterative
   (:require [twotree.core :refer [combine-on-edge combine-on-face compute-degrees]]))
 
-(defmacro symmetric [label]
+(defmacro reverse-label [label]
   `(let [[a1# a2# a3# a4# a5# a6# a7#] ~label]
      [a1# a2# a5# a6# a3# a4# a7#]))
+
+(defn compute-label-edge [a b EdgeLabels]
+  (let [reversed (< b a)
+        key (if reversed [b a] [a b])
+        labels (get EdgeLabels key)]
+    (if labels
+      (let [label (combine-on-edge (persistent! labels))]
+        [(if reversed (reverse-label label) label) key])
+      [[1 1 0 0 0 0 0] false])))
 
 (defn longest-path-iterative [tree]
   (loop [data tree
@@ -18,17 +27,8 @@
           degV (dec (get degrees v))
           addU (= 2 degU)
           addV (= 2 degV)
-          [label1 match1] (if-let [normal (get EdgeLabels [u w])]
-                            [(combine-on-edge (persistent! normal)) [u w]]
-                            (if-let [reversed (get EdgeLabels [w u])]
-                              [(symmetric (combine-on-edge (persistent! reversed))) [w u]]
-                              [[1 1 0 0 0 0 0] false]))
-          [label2 match2] (if-let [normal (get EdgeLabels [w v])]
-                            [(combine-on-edge (persistent! normal)) [w v]]
-                            (if-let [reversed (get EdgeLabels [v w])]
-                              [(symmetric (combine-on-edge (persistent! reversed))) [v w]]
-                              [[1 1 0 0 0 0 0] false]))
-
+          [label1 match1] (compute-label-edge u w EdgeLabels)
+          [label2 match2] (compute-label-edge w v EdgeLabels)
           label (combine-on-face label1 label2)]
       (if (= 1 degU degV)
         (first label)
