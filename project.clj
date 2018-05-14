64ac0ff94569ea42a4dfdd3c22cbd52a961b2bb5
Jordan Biserkov <biserkov@gmail.com>
2018-05-14 12:18:17 +0300

    Add URL and description to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject longest-path "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for computing longest paths in 2-trees in O(n) time"
+  :url "https://github.com/Biserkov/twotree-longest-path"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
