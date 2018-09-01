ddc666497cc882879fc01fdab86558d26b63d5c4
Jordan Biserkov <biserkov@gmail.com>
2018-09-01 21:00:37 +0300

    Move generators to src.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [criterium "0.4.4"]
                  [org.clojure/data.int-map "0.2.4"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [com.gfredericks/test.chuck "0.2.6"]]}
